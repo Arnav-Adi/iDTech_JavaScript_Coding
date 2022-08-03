9b51d343edad1a9381ce1dd25502e7ead9736c16
Arnav-Adi <arnava2025@gmail.com>
2022-08-03 11:20:02 -0400

    Story time

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,29 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = "alex";
+let weather = "snowing";
+let title = "a cool story";
+let city = "magic-land";
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 100000;
+let year = 900808;
+let century = 9000;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ["food", "magic wand", "sword"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasWeapons = "true;"
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("hello, my name is" + name + ".");
+console.log("I live in" + city + "where it is currencly" + weather);
+console.log("i am" + age + "because people are older here");
+console.log("it is also the year" + year + ".");
+console.log("many people ask if I have a sword, and to that I say" + hasWeapons);
+console.log("in fact, here is a list of what i carry:" + items);
